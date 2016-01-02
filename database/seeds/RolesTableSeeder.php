dd9157517c6efd857c33b9224b1e90b75cfe2271
ssi-anik <sirajul.islam.anik@gmail.com>
2016-01-02 15:26:52 +0600

    Added roles seeder

---
+++ database/seeds/RolesTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Role;
+
+class RolesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+		$roleDescriptions = ['Store Owner', 'Supervisor', 'Inventory Manager', 'Customer Service', 'Drop/Shipper', 'Accountant', 'Call center/no update', 'Store owner excluding user management', 'Only manage users', ];
+		$roleNames = ['OWNER', 'SUPR', 'INVM', 'OPER', 'VEND', 'ACCT', 'CSR', 'OWNERX', 'USERM',];
+		$index = 0;
+		foreach($roleDescriptions as $roleDescription){
+			$role = new Role();
+			$role->name = $roleNames[$index];
+			$role->display_name = $roleDescription;
+			$role->save();
+			
+			$index++;
+		}
+    }
+}
