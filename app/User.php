e8ad72f2d660a2467398b2c5f567df51f91a96cc
ssi-anik <sirajul.islam.anik@gmail.com>
2016-01-02 15:51:34 +0600

    Accessor added to User Model

--- app/User.php
+++ app/User.php
@@ -38,4 +38,9 @@ class User extends Model implements AuthenticatableContract,
      * @var array
      */
     protected $hidden = ['password', 'remember_token'];
+
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = bcrypt($value);
+    }
 }
